b36db5b5b29ee7a8ab5e46a9f95082a1a0536044
Ezekude Francis <frankizuchukwu094@gmail.com>
2022-08-16 21:40:12 +0100

    change

--- camera.py
+++ camera.py
@@ -4,6 +4,6 @@ import cv2
 cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
-    cv2.imshow("Red cam", frame)
+    cv2.imshow("my cam", frame)
     if cv2.waitKey(10) == ord("q"):
         break
